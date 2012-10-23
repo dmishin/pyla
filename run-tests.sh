37b10d6ed2b9f043e35ee4f0df3e94270edb7d24
Dmishin <shintyakov@gmail.com>
2012-10-23 20:10:00 +0400

    Better test runner + setup.py

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for test_name in pyla/test/test_*.py; do
-    echo Running $test_name
-    PYTHONPATH=PYTHONPATH:./ python2 $test_name
-done;
+python2 -m unittest discover
+
+#for test_name in pyla/test/test_*.py; do
+#    echo Running $test_name
+#    PYTHONPATH=PYTHONPATH:./ python2 $test_name
+#done;
---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup( name="PyLa",
+       version = "0.1",
+       description = "Pure Python linear algebra",
+       author = "Dmitry Shintyakov",
+       author_email="shintyakov@gmail.com",
+       url="http://github.com/dmishin/pyla",
+       packages = ["pyla"]
+       )
