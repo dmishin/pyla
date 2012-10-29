6b6887b5d413194a613555ac14386a37b304d4f8
Dmitry Shintyakov <shintyakov@gmail.com>
2012-10-29 19:49:13 +0400

    expms - fast evaluation of exp(t*M)

--- pyla/expm.py
+++ pyla/expm.py
@@ -29,8 +29,8 @@ def _exp_pade_coeffs( q, context=FloatContext ):
 
 def matrix_powers(m):
     """Generates infinite sequence of matrix powers, starting from m^0"""
-    yield eye(len(m))
-    yield m
+    yield eye(len(m)) # m^0
+    yield m           # m^1 
     m_i = mmul( m, m )
     while True:
         yield m_i
@@ -54,3 +54,29 @@ def expm( m, context=FloatContext, order=7 ):
 
     return solve( den, num )
                              
+def expms( m, context=FloatContext, order=7 ):
+    """Calculate quickly exp(t*A), for different t.
+    Takes matrix M and returns function f(t) = exp(t*M).
+    
+    Each call to that function requires no matrix multiplications and several additions
+    """
+    a = _exp_pade_coeffs( order )
+    n,n_ = shape_mat( m )
+    assert( n == n_ )
+    
+    #Prepare list of m powers and Pade coefficients
+    m_a_list = zip( matrix_powers(m), a )
+
+    def calculate_expm(t):
+        num = zeros(n,n)
+        den = zeros(n,n)
+        s = 1
+        k = context.one # t^n is stored here
+        for m_i, ai in m_a_list:
+            aik = ai*k
+            mat_combine_inplace( num, m_i, aik )
+            mat_combine_inplace( den, m_i, s*aik)
+            s = -s
+            k *= t
+        return solve( den, num )
+    return calculate_expm
